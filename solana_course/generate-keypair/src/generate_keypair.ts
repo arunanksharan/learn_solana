9f4fe4133fc1ce4f0c005d13d8a64b03d187f4bc
arunanksharan <arunank.sharan@gmail.com>
2023-10-13 00:03:56 +0530

    First Module: generate-keypair completed

---
+++ solana_course/generate-keypair/src/generate_keypair.ts
@@ -0,0 +1,15 @@
+import * as dotenv from "dotenv";
+import { Keypair } from "@solana/web3.js";
+import {getKeypairFromEnvironment} from "@solana-developers/node-helpers";
+
+dotenv.config({path: '../.env'});
+
+const loaded_keypair = getKeypairFromEnvironment("SECRET_KEY");
+console.log("Loaded Keypair Successfully from env file!")
+console.log(`PublicKey: ${loaded_keypair.publicKey.toBase58()}`);
+console.log(`Private Key: ${loaded_keypair.secretKey.toString()}`);
+
+// const new_keypair = Keypair.generate();
+// console.log("Generated Keypair Successfully!")
+// console.log(`PublicKey: ${new_keypair.publicKey.toBase58()}`);
+// console.log(`Private Key: ${new_keypair.secretKey.toString()}`);
